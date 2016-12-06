41e5555081e3d8224e4f1eed088ab3b26b5d638f
sebastian thielke <seb@redboatopera.com>
2016-12-06 01:54:15 +0100

    tests with different routes and datatypes

--- app.py
+++ app.py
@@ -8,6 +8,32 @@ app = Flask(__name__)
 def hello_world():
 	return "Hello World!"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correctr"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correctt"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correcth"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name),200
+	else:
+		return "File not found", 404
+
 if __name__ == "__main__":
 	app.run()
 
